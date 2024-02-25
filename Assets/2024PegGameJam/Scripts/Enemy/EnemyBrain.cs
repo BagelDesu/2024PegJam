e5f0dfcc10485d5c43f7e0bd6a491b1e884dc993
BagelDesu <maquinodesign@gmail.com>
2024-02-24 19:39:48 -0600

    Created Wasp testing area and wasp scripts

---
+++ Assets/2024PegGameJam/Scripts/Enemy/EnemyBrain.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State
+{
+    Patrol,
+    Chase,
+    Attack,
+}
+public class EnemyBrain : MonoBehaviour
+{
+    private Vector2 patrolStartPoint;
+    private Vector2 patrolEndPoint;
+
+
+}
