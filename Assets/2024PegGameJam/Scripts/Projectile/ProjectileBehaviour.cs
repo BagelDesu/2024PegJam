ba2a77e4c7cc4ca715de6f767fcd9be9294f6de4
eugenca <wertas15@gmail.com>
2024-02-24 19:08:52 -0600

    Projectiles version 1

--- Assets/2024PegGameJam/Scripts/Projectile/ProjectileBehaviour.cs
+++ Assets/2024PegGameJam/Scripts/Projectile/ProjectileBehaviour.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
 
 public class ProjectileBehaviour : MonoBehaviour
@@ -9,18 +10,20 @@ public class ProjectileBehaviour : MonoBehaviour
     public GameObject Instigator { get; set; }
 
     [field: SerializeField]
-    public bool IsGravityAffected { get; set; }
-
+    public bool IsGravityAffected { get; set; } = true;
 
     [field: SerializeField]
     public Vector2 InitialDirection { get; set; }
 
-    [field: SerializeField]
-    public Vector2 InitialSpeed { get; set; }
 
     [field: SerializeField]
     public float Damage { get; set; }
+    [field: SerializeField]
+    public float Impulse { get; set; }
 
+
+    [field: SerializeField]
+    public float DieDelay { get; set; } = 1.0f;
 
     private Rigidbody2D projectileRigidbody;
 
@@ -43,15 +46,47 @@ public class ProjectileBehaviour : MonoBehaviour
             Destroy(gameObject);
             return;
         }
+        //Quaternion.LookRotation(targetVector, Vector3.forward);
+
+        Vector3 movingDirection = projectileRigidbody.velocity.normalized;
+        Quaternion targetRotation = Quaternion.FromToRotation(Vector3.right, movingDirection);
+
+        transform.rotation = targetRotation;
+
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log(collision.gameObject);
+
+        if (collision.gameObject == Instigator
+            || collision.gameObject.GetComponent<ProjectileBehaviour>() != null)
+        {
+            return;
+        }
+
+
         if (collision.gameObject.TryGetComponent(out IDamageable damageable))
         {
             damageable.MakeDamage(Damage, gameObject);
             // todo: play hit animation
         }
+        StartCoroutine(DieAfterDelay(DieDelay));
+    }
+
+    IEnumerator DieAfterDelay(float time)
+    {
+        yield return new WaitForSeconds(time);
+
         Destroy(gameObject);
+    }
+
+    public void Apply()
+    {
+        Debug.Log(string.Format("Spawned, position: {0}, rotation: {1}, damage: {2}", transform.position, InitialDirection, Damage));
+        Debug.DrawLine(transform.position, transform.position + new Vector3(InitialDirection.x, InitialDirection.y, transform.position.z) * 3, Color.green, 3f);
+
+        projectileRigidbody.AddForce(InitialDirection.normalized * Impulse, ForceMode2D.Impulse);
     }
 }
